d88620515201c2678460548713932210975e62f4
Navadeep Ganesh U <navadeepganesh.ngu@gmail.com>
2022-05-02 19:36:46 +0530

    implemented bubble sorting in asm

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # ARM_Assembly_Keil
  Working on ARM assembly with Cortex M4 - STM32F407 DISCO
+
+Resources:     
+> https://www.rose-hulman.edu/class/csse/csse132/1718a/  
+> https://www.csie.ntu.edu.tw/~cyy/courses/assembly/08fall/lectures/handouts/  
+> http://www.rigwit.co.uk/ARMBook/  
+> https://www.youtube.com/playlist?list=PL-ftFcielQtGxBUfzkbz9tWlRZb-rlJ2p  
--- bubble_sort.s
+++ bubble_sort.s
@@ -1,4 +1,6 @@
 ;;;bubble sorting in ascending order - ARM assembly
+;;;specify the data size 'n' in line 16 
+;;;enter the data from 0x20000001 to 0x2000000n
 
 ;declare myData area for DATA
 	AREA	myData,	DATA
@@ -12,41 +14,50 @@ SRAM_BASE_ADDR	EQU	0x20000000
 
 __main
 	LDR		R0, =SRAM_BASE_ADDR
-	MOV		R1, #8			;n = number of elements in dataset
+	ADD		R1, R0, #8  	;n = 8 no. of elements in dataset. Last element address offset (0x20000001 to 0x20000008)
+	MOV		R2, R0			;i = start at 0th index
+	MOV		R4, #0			;set flag once sorting is done
+	SUB		R7, R1, #1		;(n-1)
+	
+iterate_loop1
+	MOV		R3, R0			;j = always start at 0th index
+	
+	SUB		R8, R7, R2		;(R1-1)-R2 
+	ADD		R9, R0, R8		;added base address offset which is lost after subtracting R1-R2; now(n-i-1) 
+
+	CMP		R2, R7			;i < n-1
+	BLT		loop1			;branch if less than
+	B		setflag			;sorting done
 	
 loop1
-loop2
-	MOV		R2, #0			;i
-	MOV		R3, #0			;j
-	MOV		R4, #0			;flag
-	SUB		R7, R1, #1		;R1-1 (n-1)
-	LDRB	R8, [R9]		;instantaneous R1-R2-1 (n-i-1)
-	SUB		R9, R7, R2		;(R1-1)-R2
-	ADD		R10, R3, #1		;j+1
-	
-	;start with SRAM base address
-	;increment R2 till (R1-1)th element
-	;start with SRAM base address inside the previous iteration
-	;incrementation R3 till (R1-R2-1)th element
-	
 	ADD		R2, R2, #1		;i++
-	CMP		R2, R7			;i < n-1
-	BNE		loop1
+
+iterate_loop2
+	CMP		R3, R9			;j < n-i-1
+	BLT		loop2
+	B		iterate_loop1
 	
+loop2
 	ADD		R3, R3, #1		;j++
-	CMP		R3, R8			;j < n-i-1
-	BNE		loop2
 	
-	;compare and if data[R3] > data[R3+1]
+	;compare and if data[R3] > data[R3+1]	
 	LDRB	R5, [R3]
-	LDRB	R6, [R10]
+	LDRB	R6, [R3, #1]	;j+1
 	CMP		R5, R6
-	BNE		swap
+	BGT		swap
+	B		return
+	
+return
+	B		iterate_loop2
 	
 swap
 	ADD		R5,  R5, R6
-	SUB		R10, R5, R6
+	SUB		R6,  R5, R6
 	SUB		R5,  R5, R6
+	
+	STRB	R5, [R3]		;store the swapped R5 value !!caution!! use STRB not STR
+	STRB	R6, [R3, #1]	;store the swapped R6 value
+	B 		return
 	
 setflag
 	MOV		R4, #1			;set the flag
